537e08409d3cedb8487144383aac204a6655ff2d
David Gageot <david@gageot.net>
2014-06-03 23:28:14 +0200

    Update webapp

--- src/main/java/net/gageot/MainHello.java
+++ src/main/java/net/gageot/MainHello.java
@@ -4,6 +4,6 @@ import net.codestory.http.*;
 
 public class MainHello {
   public static void main(String[] args) {
-    new WebServer(routes -> routes.catchAll("Hello World")).start();
+    new WebServer().start();
   }
 }
