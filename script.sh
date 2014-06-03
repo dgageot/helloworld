52e8070bf8cd2636f8c94cc769cf8d6748640ca4
David Gageot <david@gageot.net>
2014-06-03 13:08:01 +0200

    Script for easy copy paste

--- script.sh
+++ script.sh
@@ -1,30 +1,15 @@
-docker build -t dgageot/helloworld github.com/dgageot/helloworld
+----------------------------------
 
-docker run -i -t dgageot/helloworld /bin/bash
-
-docker run -p 80:8080 -t -i dgageot/helloworld java -jar target/hello.jar
-
-gcutil \
-	--service_version=v1 \
-	--project=numeric-scope-568 \
-addinstance hello \
-	--zone=europe-west1-b \
-	--machine_type=n1-standard-1 \
-	--tags=http-server \
-	--image="https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/backports-debian-7-wheezy-v20140415"
-
-gcutil \
-	--service_version=v1 \
-	--project=numeric-scope-568 \
-ssh \
-	--zone=europe-west1-b \
-hello
+gcloud compute instances create jug --image projects/google-containers/global/images/container-vm-v20140522 --metadata-from-file google-container-manifest=containers.yaml --zone europe-west1-b --machine-type f1-micro --tags=http-server
 
 curl get.docker.io | bash
 
-gcloud compute instances create jug \
-    --image projects/google-containers/global/images/container-vm-v20140522 \
-    --metadata-from-file google-container-manifest=containers.yaml \
-    --zone europe-west1-b \
-    --machine-type f1-micro \
-    --tags=http-server 
+gcutil --service_version=v1 --project=numeric-scope-568 ssh --zone=europe-west1-b hello
+
+gcutil --service_version=v1 --project=numeric-scope-568 addinstance hello --zone=europe-west1-b --machine_type=n1-standard-1 --tags=http-server --image="https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/backports-debian-7-wheezy-v20140415"
+
+docker run -p 80:8080 -t -i dgageot/helloworld java -jar target/hello.jar
+
+docker run -i -t dgageot/helloworld /bin/bash
+
+docker build -t dgageot/helloworld github.com/dgageot/helloworld
